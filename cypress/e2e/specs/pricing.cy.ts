b8968669378646c417dcb1f164463df305fa96fb
Yulia Bocharova <panterka13@yandex.ru>
2023-03-30 15:06:50 +0200

    E2e tests with github actions

--- cypress/e2e/specs/pricing.cy.ts
+++ cypress/e2e/specs/pricing.cy.ts
@@ -2,14 +2,56 @@
 
 // This file will contain tests for the Pricing page. Feel free to add as many as you want.
 
-context('Pricing', () => {
-    beforeEach(() => {
-        cy.visit('https://www.smartlook.com/pricing/?currencyCode=CZK')
+describe('Pricing page', () => {
+    context('Plans', () => {
+        beforeEach(() => {
+            cy.visit('https://www.smartlook.com/pricing/?currencyCode=CZK')
+            // Accepting all cookies in pop up window
+            cy.get('#onetrust-accept-btn-handler').should('contain', 'Accept All Cookies').click()
+            // Check iframe
+            cy.get('#pricing-page-iframe', { timeout: 20000 }).should('be.visible')
+        })
+
+        // I found that iframe with id = "pricing-page-iframe" is recursive in cypress chrome browser
+        // I tried to find elements inside the frame but it's not possible and this code doesn't work
+        it('Check plans labels', () => {
+            cy.get("iframe[id='pricing-page-iframe']")
+              .its('0.contentDocument.body')
+              .should('be.visible')
+              .then(cy.wrap)
+              .xpath("//*[@id='__next']/div/main/div[2]/div/div[1]/div[1]/div[1]/h5")
+              .should('contain', 'Free plan')
+        })
+        // So I will describe the testing process by words
+
+
+
+
+        // First task: Check prices of packages
+        
+        // Tree plans should be present on pricing page
+        // Each of them contains its own price
+        // Prices are the same as on the top content as on the comparing table below
+
+
+
+
+        // Second task: Test that 'Build a plan' button works and the modal is behaving as expected - prices and limits
+
+        // 'Build a plan' button should opened the modal
+        // The modal contains monthly session options, Billing period area, info area and Continue, Close buttons
+        // Choose any monthly session option, session limit and subtotal sum in billing period should be changed according a logic
+        // It's possible to change billing period from monthly to annualy or choose a currency (USD, EUR, CZK) where CZK is set by default
+        // Click on 'Continue' button
+        // Billing period area should stayed the same
+        // There are three additional features displayed 
+        // Click on 'Add' button for any of them
+        // Billing period area should be changed by adding Add-ons field and subtotal sum should be increased accordingly
+        // Click on 'Start your free trial' button
+        // Redirect to https://app.smartlook.com/sign/up
+
+        // I will test also 'Close', 'Remove' and 'Back' buttons. 
+        // Also will play with options of billing period and monthly session options
+        // Check that by clicking on each sentence in info area it will display needed information.
     })
-
-    // Check prices of packages
-
-    // Test that 'Build a plan' button works and the modal is behaving as expected - prices and limits
-
-    // Feel free to modify this file and other files to you liking except the url.
 })
--- cypress/e2e/specs/signIn.cy.ts
+++ cypress/e2e/specs/signIn.cy.ts
@@ -3,15 +3,164 @@
 // This file will contain tests for the Sign In form. Feel free to add as many as you want.
 // You dont have to test successful sign in as an account is needed for it. Other scenarios are recommended to test.
 
-context('Sign in', () => {
-    beforeEach(() => {
-        cy.visit('https://app.alfa.smartlook.cloud/sign/in')
+describe('Sign-in page', () => {
+    context('Visability of elements on sign-in page', () => {
+        beforeEach(() => {
+            cy.visit('https://app.alfa.smartlook.cloud/sign/in')
+        })
+    
+        it('Presence all elements on page', () => {
+            cy.get('h1').should('contain', 'Log in to your account')
+            cy.get('#sign-google-login-button').should('exist')
+            cy.get('#sign-facebook-login').should('exist')
+            cy.get('#sign-sso-login-button').should('exist')
+            cy.get('#sign-in-input-email--content').should('exist')
+            cy.get('#sign-in-input-password--content').should('exist')
+            cy.get('#sign-in-button').should('exist')
+            cy.get('#sign-in_sign-up-link').should('exist')
+            cy.get('#sign-in_forgot-password-link').should('exist')
+            cy.get('h4').should('contain', 'Trusted by 2800+ brands')
+            cy.get('#google-policy').should('exist')
+            cy.get('#google-tos').should('exist')
+        })
+
+        it('Check labels for buttons and inputs', () => {
+            cy.get('#sign-google-login-button > div > div > span').should('contain', 'Log in with Google')
+            cy.get('#sign-facebook-login > div').should('contain', 'Log in with Facebook')
+            cy.get('#sign-sso-login-button > div').should('contain', 'Log in with SSO')
+            cy.get('#sign-in-input-email--label').should('contain', 'Email')
+            cy.get('#sign-in-input-password--label').should('contain', 'Password')
+            cy.get('#sign-in-button > div').should('contain', 'Log in')
+        })
     })
 
-    // Test sign in form
-    it('Sign in test case', () => {
-        // Fill in some tests
+    context('Input sign-in form test cases', () => {
+        beforeEach(() => {
+            cy.visit('https://app.alfa.smartlook.cloud/sign/in')
+        })
+    
+        it('Input correct credentials without login', () => {
+            cy.get('#sign-in-input-email--content').type('test@email.com')
+            cy.get('#sign-in-input-email--inner').should('have.value', 'test@email.com')
+            cy.get('#sign-in-input-password--content').type('testPassword')
+            cy.get('#sign-in-input-password--inner').should('have.value', 'testPassword')
+            
+        })
+
+        it('Non-existing user', () => {
+            cy.get('#sign-in-input-email--content').type('test@email.com')
+            cy.get('#sign-in-input-password--content').type('testPassword')
+            cy.get('#sign-in-button').click()
+            cy.get('span')
+              .should('contain', 'Incorrect login or password. If you recently signed up, please check your email for your invitation link.')
+        })
+
+        it('Correct email and empty password', () => {
+            cy.get('#sign-in-input-email--content').type('test@email.com')
+            cy.get('#sign-in-button').click()
+            cy.get('#sign-in-input-password')
+               .next()
+               .should('have.text', 'Your password must be at least 6 characters long')
+        })
+
+        it('Correct email and invalid password format', () => {
+            cy.get('#sign-in-input-email--content').type('test@email.com')
+            cy.get('#sign-in-input-password--content').type('test')
+            cy.get('#sign-in-button').click()
+            cy.get('#sign-in-input-password')
+               .next()
+               .should('have.text', 'Your password must be at least 6 characters long')
+        })
+
+        it('Correct password and empty email', () => {
+            cy.get('#sign-in-input-password--content').type('test')
+            cy.get('#sign-in-button').click()
+            cy.get('#sign-in-input-password')
+               .next()
+               .should('have.text', 'Your password must be at least 6 characters long')
+        })
+
+        // Suggestion - use a new validation message which specifies correct format
+        it('Correct password and invalid email format', () => {
+            cy.get('#sign-in-input-email--content').type('test')
+            cy.get('#sign-in-input-password--content').type('test')
+            cy.get('#sign-in-button').click()
+            cy.get('#sign-in-input-email')
+               .next()
+               .should('have.text', 'Please fill in your email address')
+        })
+    
+        // Suggestion - display validation messages for empty email and password together when user click on Log in button
+        it('Empty credentials', () => {
+            cy.get('#sign-in-button').click()
+            cy.get('#sign-in-input-email')
+               .next()
+               .should('have.text', 'Please fill in your email address')
+        })
     })
 
-    // Feel free to modify this file and other files to you liking except the url.
+    context('Show/hide password options', () => {
+        beforeEach(() => {
+            cy.visit('https://app.alfa.smartlook.cloud/sign/in')
+        })
+
+        it('Check option to hide/show password', () => {
+            //password is hidden
+            cy.get('#sign-in-input-password--inner')
+              .type('testPassword')
+              .should('have.attr', 'type', 'password')
+
+            //click on show password image
+            cy.get('#sign-in-input-password--content').click(320, 30)
+
+            //password is shown
+            cy.get('#sign-in-input-password--inner')
+              .should('have.attr', 'type', 'text')
+        })
+    })
+
+    context('Other login variants', () => {
+        beforeEach(() => {
+            cy.visit('https://app.alfa.smartlook.cloud/sign/in')
+        })
+
+        it('Google login', () => {
+            cy.get('#sign-google-login-button').click()
+            // test popup
+        })
+
+        it('Facebook login', () => {
+            cy.get('#sign-facebook-login').click()
+            // test popup
+        })
+
+        it('SSO login', () => {
+            cy.get('#sign-sso-login-button').click()
+            cy.url().should('eq', 'https://app.alfa.smartlook.cloud/sign/sso')
+        })
+    })
+
+    context('Links on sign-in page', () => {
+        beforeEach(() => {
+            cy.visit('https://app.alfa.smartlook.cloud/sign/in')
+        })
+    
+        it('Link: Sign-up', () => {
+            cy.get('#sign-in_sign-up-link').click()
+            cy.url().should('eq', 'https://app.alfa.smartlook.cloud/sign/up')
+        })
+
+        it('Link: I forgot my password', () => {
+            cy.get('#sign-in_forgot-password-link').click()
+            cy.url().should('eq', 'https://app.alfa.smartlook.cloud/sign/reset-password')
+        })
+
+        it('Link: Google privacy policy', () => {
+            cy.get('#google-policy').should('have.attr', 'href', 'https://policies.google.com/privacy')
+        })
+
+        it('Link: Terms of service', () => {
+            cy.get('#google-tos').should('have.attr', 'href', 'https://policies.google.com/terms')
+        })
+    })
 })
